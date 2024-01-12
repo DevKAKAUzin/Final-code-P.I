d78d336970a7a7e4075952415f6c3b7a2dee43a1
Ricksant31 <henrique.santana0910@gmail.com>
2024-01-12 11:45:01 -0300

    Plan1Incorporado

--- Código P.I/script.js
+++ Código P.I/script.js
@@ -1,20 +1,19 @@
 const handleSubmit = (event) => {
-    event.preventDefault();
-    
-    const name = document.querySelector('input[name=nome]').value;
-    const email = document.querySelector('input[name=email]').value;
-    const sobrenome = document.querySelector('input[name=sobrenome]').value;
+  event.preventDefault();
+  
+  const name = document.querySelector('input[name=nome]').value;
+  const email = document.querySelector('input[name=email]').value;
+  const numero = document.querySelector('input[name=numero]').value;
 
-    fetch('https://api.sheetmonkey.io/form/4YmoR5EYr2xLxj3ec6VjHs', {
+  fetch('https://api.sheetmonkey.io/form/4YmoR5EYr2xLxj3ec6VjHs', {
 
-    method: 'post',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({name, email, sobrenome }),
-    }).then(()=> alert('dados salvos'));
-    }
-    
-    document.querySelector('form').addEventListener('submit', handleSubmit);
-    
+  method: 'post',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({name, email, numero }),
+  }).then(()=> alert('dados salvos'));
+  }
+  
+  document.querySelector('form').addEventListener('submit', handleSubmit);
